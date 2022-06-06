d8bee697d1b3e37bfe0105f8ab7727ec783b97bd
Alberto Domingues <94317971+albertodomingues81@users.noreply.github.com>
2022-06-06 15:50:08 +0000

    Initial commit

---
+++ Variaveis.py
@@ -0,0 +1,19 @@
+# 1 . Variaveis
+
+idade = 26 #Criando uma variável
+
+print(idade)
+
+nome = 'Alberto Domingues'
+
+print(nome)
+
+idade = 26 #int
+altura = 1.77 #float
+nome = 'Alberto Domingues' #string
+estudando = True # bool
+
+print( type(idade) )
+print( type(altura) )
+print( type(nome) )
+print( type(estudando) )
